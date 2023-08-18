4fbc1fb25d9ea593d87450275fc7e31daac22253
Kevin Nguyen <kevinnguyen1413@gmail.com>
2023-08-19 01:31:35 +0700

    A text generator that takes user inputs to turn into a paragraph

---
+++ madlibs.py
@@ -0,0 +1,12 @@
+
+adjective1 = input('Adjective1: ')
+past_tense_verb1 = input('Past Tense Verb1: ')
+continuous_verb1 = input('Continuous Verb1: ')
+noun1 = input('Noun1: ')
+adjective2 = input('Adjective2: ')
+noun2 = input('Noun2: ')
+
+
+print(f'This is absolutely {adjective1}! It is all over the news! A man named Jack {past_tense_verb1} his entire family, consisting of \
+his wife and son. Authorities are {continuous_verb1} the incident. The neighborhood is shocked by his {noun1}. They could not believe \
+that he is capable of such a {adjective2} {noun2}.')
